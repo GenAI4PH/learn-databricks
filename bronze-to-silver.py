b08ac1b092098b14b9e198cf2fe13c087abc4856
GenAI4PH <kmp4@njit.edu>
2026-05-24 19:07:11 +0000

    added product aggregation

--- bronze-to-silver.py
+++ bronze-to-silver.py
@@ -1,2 +1,10 @@
-df = spark.read.table("table name here")
+#This is notebook to read sales and customer from sample databricks table
+df1 = spark.read.table("samples.bakehouse.sales_transactions")
+df2 = spark.read.table("samples.bakehouse.sales_customers")
+from pyspark.sql.functions import sum, desc, col
+df = df1.join(df2, "customerID").select("first_name", "last_name", "product", "quantity", "unitPrice", col("totalPrice").cast("float"))\
+    .groupBy("product")\
+    .agg(sum("totalPrice").alias("totalSales"))\
+    .orderBy(desc("totalSales"))
+
 display(df)
