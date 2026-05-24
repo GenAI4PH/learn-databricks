5d293626d5eef5a0cc452550272c40d77a1100aa
GenAI4PH <156859431+GenAI4PH@users.noreply.github.com>
2026-05-24 14:58:32 -0400

    Update bronze-to-silver.py
    added display

--- bronze-to-silver.py
+++ bronze-to-silver.py
@@ -2,4 +2,4 @@
 df1 = spark.read.table("samples.bakehouse.sales_transactions")
 df2 = spark.read.table("samples.bakehouse.sales_customers")
 df = df1.join(df2, "customerID").select("first_name", "last_name", "product", "quantity", "unitPrice","totalPrice")
-show(df)
+display(df)
