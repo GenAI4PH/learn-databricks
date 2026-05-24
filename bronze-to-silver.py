e284c69636500280ed4e1bfc5ae4feba9e45ef46
GenAI4PH <156859431+GenAI4PH@users.noreply.github.com>
2026-05-24 14:51:19 -0400

    Update bronze-to-silver.py

--- bronze-to-silver.py
+++ bronze-to-silver.py
@@ -1,4 +1,5 @@
+#This is notebook to read sales and customer from sample databricks table
 df1 = spark.read.table("samples.bakehouse.sales_transactions")
 df2 = spark.read.table("samples.bakehouse.sales_customers")
 df = df1.join(df2, "customerID").select("first_name", "last_name", "product", "quantity", "unitPrice","totalPrice")
-display(df)
+show(df)
