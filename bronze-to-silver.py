a4de783c48a8f1fccbad38f4e95729596e4f1667
GenAI4PH <kmp4@njit.edu>
2026-05-24 19:05:14 +0000

    added product aggredation

--- bronze-to-silver.py
+++ bronze-to-silver.py
@@ -1,5 +1,10 @@
 #This is notebook to read sales and customer from sample databricks table
 df1 = spark.read.table("samples.bakehouse.sales_transactions")
 df2 = spark.read.table("samples.bakehouse.sales_customers")
-df = df1.join(df2, "customerID").select("first_name", "last_name", "product", "quantity", "unitPrice","totalPrice")
+from pyspark.sql.functions import sum, desc, col
+df = df1.join(df2, "customerID").select("first_name", "last_name", "product", "quantity", "unitPrice", col("totalPrice").cast("float"))\
+    .groupBy("product")\
+    .agg(sum("totalPrice").alias("totalSales"))\
+    .orderBy(desc("totalSales"))
+
 display(df)
